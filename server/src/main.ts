0028aa165cb79462b13a04efcf0a9bef01c4cee4
Charles B. Capagngan <cbcapagngan2@student.apc.edu.ph>
2026-02-23 06:01:47 +0000

    dawwad

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'https://personal-website-finals-d58r.vercel.app', // Your actual frontend URL
+    // This allows any of your Vercel deployments to talk to the backend
+    origin: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
